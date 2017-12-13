924498d30d5b5ebddbd7e24c5f4bc95cb882dffe
Sahil Malhotra <sahilmalhotra578@gmail.com>
2017-12-13 15:51:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,20 @@
 
 #python script
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+# Replace below path with the absolute path
+# to chromedriver in your computer
+driver = webdriver.Chrome('/home/saket/Downloads/chromedriver')
+
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
+
+# Replace 'Friend's Name' with the name of your friend 
+# or the name of a group 
+target = '"Friend\'s Name"'
